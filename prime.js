c2c25f013dcd1b52291f49424000dcd2d4eaf62b
Carmezim <carmezim.filho@gmail.com>
2017-04-10 22:11:32 +0100

    is prime?

---
+++ prime.js
@@ -0,0 +1,20 @@
+const isPrime = (n) => {
+  let divisor = 2;
+
+  while (n > divisor) {
+    if(n % divisor === 0) {
+      return false;
+    }
+    else {
+      divisor++;
+    }
+  }
+  return true;
+}
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(3));
+console.log(isPrime(4));
+console.log(isPrime(51));
+console.log(isPrime(73));
+console.log(isPrime(42));
