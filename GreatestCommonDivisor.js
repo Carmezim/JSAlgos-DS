b17944241de4cbf30894557b96cb960d9978c131
Carmezim <carmezim.filho@gmail.com>
2017-04-25 19:57:08 +0100

    bad greatest common divisor

--- GreatestCommonDivisor.js
+++ GreatestCommonDivisor.js
@@ -4,4 +4,26 @@ const greatestCommonDivisor = (a, b) => {
   else
     return greatestCommonDivisor(b, a % b);
 }
-console.log(greatestCommonDivisor(6, 18));
+//console.log(greatestCommonDivisor(512, 32));
+
+
+
+// lame greatestCommonDivisor
+const lameGreatestCommonDivisor = (a, b) => {
+  let divisor = 2;
+  let greatestDivisor = 1;
+
+  if (a < 2 || b < 2) {
+    return 1;
+  }
+  
+  while (a >= divisor && b >= divisor) {
+    if(a % divisor == 0 && b % divisor == 0) {
+      greatestDivisor = divisor;
+    }
+    divisor++;
+  }
+  return greatestDivisor;
+}
+
+console.log(lameGreatestCommonDivisor(512, 32));
